49014d8a3be282af2bbc9ad03f86b290ac8e00d1
Simon Hällström <simonhallstrom@live.se>
2025-08-28 01:18:33 +0200

    chore: delete unused old files

--- src/fields/admin/index.ts
+++
@@ -1,5 +0,0 @@
-import type { FieldAccess } from 'payload'
-import { checkRole } from '@/access/checkRole'
-
-// field access - admin only
-export const adminField: FieldAccess = ({ req: { user } }) => checkRole(['admin'], user)
